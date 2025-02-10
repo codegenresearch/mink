f104f46fe1c1d8b2169b5fd75e82c84d80df1795
venkatkrishna <venkatkrishna@moe>
2025-02-10 04:15:25 +0100

    Reset to 5fbf0f6aeeaf1746af88137b9a299e84687a74ce and made changes for LLM Round 1

--- docs/conf.py
+++ docs/conf.py
@@ -1,54 +1,45 @@
-from __future__ import annotations
-
 # Configuration file for the Sphinx documentation builder.
 #
-# This file configures the Sphinx documentation generator to build the documentation for the mink project.
-# For a comprehensive list of configuration options, refer to the official Sphinx documentation:
+# For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project Information -----------------------------------------------------
-# This section contains metadata about the project, which is used in various parts of the generated documentation.
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+# -- Project information -----------------------------------------------------
+project = "mink"
+copyright = "2024, Kevin Zakka"
+author = "Kevin Zakka"
 
-project: str = "mink"
-copyright: str = "2024, Kevin Zakka"
-author: str = "Kevin Zakka"
-
-# -- General Configuration ---------------------------------------------------
-# This section configures general settings for the Sphinx documentation builder.
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-# List of Sphinx extensions to enable
-extensions: list[str] = [
-    "sphinx.ext.autodoc",  # Automatically generate documentation from docstrings
-    "sphinx.ext.coverage",  # Check for documentation coverage
-    "sphinx-mathjax-offline",  # Use MathJax for rendering math equations offline
-    "sphinx.ext.napoleon",  # Support for Google and NumPy style docstrings
-    "sphinx_favicon",  # Add favicons to the documentation
+# -- General configuration ---------------------------------------------------
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.coverage",
+    "sphinx-mathjax-offline",
+    "sphinx.ext.napoleon",
+    "sphinx_favicon",
 ]
 
-# Paths to templates that the theme can use
-templates_path: list[str] = ["_templates"]
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-# List of patterns to ignore when looking for source files
-exclude_patterns: list[str] = ["_build", "Thumbs.db", ".DS_Store"]
+source_suffix = {".rst": "restructuredtext"}
 
-# Mapping of source file suffixes to file types
-source_suffix: dict[str, str] = {".rst": "restructuredtext"}
+pygments_style = "sphinx"
 
-# Style for syntax highlighting in the documentation
-pygments_style: str = "sphinx"
+napoleon_numpy_docstring = False
+napoleon_use_rtype = False
 
-# Configuration for the Napoleon extension
-napoleon_numpy_docstring: bool = False  # Do not use NumPy style docstrings
-napoleon_use_rtype: bool = False  # Do not include the return type in the docstring
+# autodoc configuration
+autodoc_typehints = "description"
+autodoc_class_signature = "separated"
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": True,
+    "show-inheritance": True,
+}
 
-# -- Options for HTML Output -------------------------------------------------
-# This section configures the HTML output options for the Sphinx documentation builder.
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+autodoc_type_aliases = {
+    # Define type aliases here if needed
+}
 
-# Theme to use for HTML and HTML Help pages
-html_theme: str = "sphinx_rtd_theme"
-
-# Name of the HTML help builder output file
-htmlhelp_basename: str = "minkdoc"
+# -- Options for HTML output -------------------------------------------------
+html_theme = "sphinx_rtd_theme"
+htmlhelp_basename = "minkdoc"
