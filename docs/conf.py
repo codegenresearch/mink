cebd555f779c458a0bd534c1f7c8dbc958374035
venkatkrishna <venkatkrishna@moe>
2025-02-11 00:52:35 +0100

    Reset to c9885f28a9f81f574c4cb89778f23937ac93622d and made changes for LLM Round 1

--- docs/conf.py
+++ docs/conf.py
@@ -1,29 +1,11 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-from pathlib import Path
-
-import toml
-
+# Project information
 project = "mink"
 copyright = "2024, Kevin Zakka"
 author = "Kevin Zakka"
 
-# The short X.Y version
-version: str = toml.load(Path(__file__).absolute().parent.parent / "pyproject.toml")[
-    "project"
-]["version"]
-if not version.isalpha():
-    version = "v" + version
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
+# General configuration
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.coverage",
@@ -52,9 +34,6 @@ autodoc_type_aliases = {
     "npt.ArrayLike": "ArrayLike",
 }
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
+# Options for HTML output
 html_theme = "sphinx_rtd_theme"
-
 htmlhelp_basename = "minkdoc"
