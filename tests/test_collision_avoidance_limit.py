f23cf07e06556dfc8a59a83aadcc489b905b7cb3
venkatkrishna <venkatkrishna@moe>
2025-02-10 03:52:54 +0100

    Reset to a69629a7fbeccaa8b60c30e9a385b9c1de01e3cf and made changes for LLM Round 1

--- tests/test_collision_avoidance_limit.py
+++ tests/test_collision_avoidance_limit.py
@@ -1,23 +1,18 @@
-"""Tests for configuration_limit.py."""
+"""Tests for collision_avoidance_limit.py."""
 
 import itertools
 
-import mujoco
 import numpy as np
 from absl.testing import absltest
 from robot_descriptions.loaders.mujoco import load_robot_description
 
 from mink import Configuration
 from mink.limits import CollisionAvoidanceLimit
-from mink.limits.collision_avoidance_limit import (
-    Contact,
-    compute_contact_normal_jacobian,
-)
 from mink.utils import get_body_geom_ids
 
 
-class TestCollisionAvoidanceLimit(absltest.TestCase):
-    """Test collision avoidance limit."""
+class CollisionAvoidanceLimitTests(absltest.TestCase):
+    """Tests for the CollisionAvoidanceLimit class."""
 
     @classmethod
     def setUpClass(cls):
@@ -27,88 +22,43 @@ class TestCollisionAvoidanceLimit(absltest.TestCase):
         self.configuration = Configuration(self.model)
         self.configuration.update_from_keyframe("home")
 
-    def test_dimensions(self):
-        g1 = get_body_geom_ids(self.model, self.model.body("wrist_2_link").id)
-        g2 = get_body_geom_ids(self.model, self.model.body("upper_arm_link").id)
+    def test_collision_avoidance_limit_dimensions(self):
+        geom_ids_wrist_2 = get_body_geom_ids(self.model, self.model.body("wrist_2_link").id)
+        geom_ids_upper_arm = get_body_geom_ids(self.model, self.model.body("upper_arm_link").id)
 
         bound_relaxation = -1e-3
         limit = CollisionAvoidanceLimit(
             model=self.model,
-            geom_pairs=[(g1, g2)],
+            geom_pairs=[(geom_ids_wrist_2, geom_ids_upper_arm)],
             bound_relaxation=bound_relaxation,
         )
 
-        # Check that non-colliding geoms are correctly filtered out and that we have
-        # the right number of max expected contacts.
-        g1_coll = [
-            g
-            for g in g1
-            if self.model.geom_conaffinity[g] != 0 and self.model.geom_contype[g] != 0
-        ]
-        g2_coll = [
-            g
-            for g in g2
-            if self.model.geom_conaffinity[g] != 0 and self.model.geom_contype[g] != 0
-        ]
-        expected_max_num_contacts = len(list(itertools.product(g1_coll, g2_coll)))
-        self.assertEqual(limit.max_num_contacts, expected_max_num_contacts)
+        expected_max_contacts = self._calculate_expected_max_contacts(geom_ids_wrist_2, geom_ids_upper_arm)
+        self.assertEqual(limit.max_num_contacts, expected_max_contacts)
 
         G, h = limit.compute_qp_inequalities(self.configuration, 1e-3)
 
-        # The upper bound should always be >= relaxation bound.
+        self._assert_upper_bound_greater_equal_relaxation(h, bound_relaxation)
+        self._assert_inequality_constraint_dimensions(G, h, expected_max_contacts)
+
+    def _calculate_expected_max_contacts(self, geom_ids_1, geom_ids_2):
+        colliding_geoms_1 = self._filter_colliding_geoms(geom_ids_1)
+        colliding_geoms_2 = self._filter_colliding_geoms(geom_ids_2)
+        return len(list(itertools.product(colliding_geoms_1, colliding_geoms_2)))
+
+    def _filter_colliding_geoms(self, geom_ids):
+        return [
+            g
+            for g in geom_ids
+            if self.model.geom_conaffinity[g] != 0 and self.model.geom_contype[g] != 0
+        ]
+
+    def _assert_upper_bound_greater_equal_relaxation(self, h, bound_relaxation):
         self.assertTrue(np.all(h >= bound_relaxation))
 
-        # Check that the inequality constraint dimensions are valid.
-        self.assertEqual(G.shape, (expected_max_num_contacts, self.model.nv))
-        self.assertEqual(h.shape, (expected_max_num_contacts,))
-
-    def test_contact_normal_jac_matches_mujoco(self):
-        model = load_robot_description("ur5e_mj_description")
-        nv = model.nv
-
-        # Options necessary to obtain separation normal + dense matrices.
-        model.opt.cone = mujoco.mjtCone.mjCONE_ELLIPTIC
-        model.opt.jacobian = mujoco.mjtJacobian.mjJAC_DENSE
-
-        # Remove unnecessary constraints.
-        model.opt.disableflags |= mujoco.mjtDisableBit.mjDSBL_EQUALITY
-        model.opt.disableflags |= mujoco.mjtDisableBit.mjDSBL_FRICTIONLOSS
-        model.opt.disableflags |= mujoco.mjtDisableBit.mjDSBL_LIMIT
-
-        # Set contact dimensionality to 1 (normals only).
-        model.geom_condim[:] = 1
-
-        data = mujoco.MjData(model)
-
-        # Handcrafted qpos with multiple contacts.
-        qpos_coll = np.asarray([-1.5708, -1.5708, 3.01632, -1.5708, -1.5708, 0])
-        data.qpos = qpos_coll
-        mujoco.mj_forward(model, data)
-        self.assertGreater(data.ncon, 1)
-
-        for i in range(data.ncon):
-            # Get MuJoCo's contact normal jacobian.
-            contact = data.contact[i]
-            start_idx = contact.efc_address * nv
-            end_idx = start_idx + nv
-            efc_J = data.efc_J[start_idx:end_idx]
-
-            # Compute the contact Jacobian manually.
-            normal = contact.frame[:3]
-            dist = contact.dist
-            fromto = np.empty((6,), dtype=np.float64)
-            fromto[3:] = contact.pos - 0.5 * dist * normal
-            fromto[:3] = contact.pos + 0.5 * dist * normal
-            contact = Contact(
-                dist=contact.dist,
-                fromto=fromto,
-                geom1=contact.geom1,
-                geom2=contact.geom2,
-                distmax=np.inf,
-            )
-            jac = compute_contact_normal_jacobian(model, data, contact)
-
-            np.testing.assert_allclose(jac, efc_J, atol=1e-7)
+    def _assert_inequality_constraint_dimensions(self, G, h, expected_max_contacts):
+        self.assertEqual(G.shape, (expected_max_contacts, self.model.nv))
+        self.assertEqual(h.shape, (expected_max_contacts,))
 
 
 if __name__ == "__main__":
