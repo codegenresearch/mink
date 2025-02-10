c5fdc8f86c6a73ab10281cffa4a5114b3afcad2a
venkatkrishna <venkatkrishna@moe>
2025-02-10 03:52:44 +0100

    Reset to 372c0d0807895860f5f90ac4cc1d0cae7b98f14c and made changes for LLM Round 1

--- tests/test_lie_utils.py
+++ tests/test_lie_utils.py
@@ -9,11 +9,20 @@ from mink.lie import utils
 class TestUtils(absltest.TestCase):
     def test_skew_throws_assertion_error_if_shape_invalid(self):
         with self.assertRaises(AssertionError):
-            utils.skew(np.zeros((5,)))
+            utils.skew(np.zeros((5,)))  # Invalid shape, should raise AssertionError
 
     def test_skew_equals_negative(self):
-        m = utils.skew(np.random.randn(3))
-        np.testing.assert_allclose(m.T, -m)
+        omega: np.ndarray = np.random.randn(3)
+        m: np.ndarray = utils.skew(omega)
+        np.testing.assert_allclose(m.T, -m, err_msg="Skew matrix is not skew-symmetric")
+
+    def test_skew_returns_correct_skew_symmetric_matrix(self):
+        omega: np.ndarray = np.array([1.0, 2.0, 3.0])
+        expected_m: np.ndarray = np.array([[0.0, -3.0, 2.0],
+                                          [3.0, 0.0, -1.0],
+                                          [-2.0, 1.0, 0.0]])
+        m: np.ndarray = utils.skew(omega)
+        np.testing.assert_allclose(m, expected_m, err_msg="Skew matrix does not match expected output")
 
 
 if __name__ == "__main__":
