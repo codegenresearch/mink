fe5b94b30622fa025b36931da3dbe39a1f1fd41d
venkatkrishna <venkatkrishna@moe>
2025-02-10 06:36:58 +0100

    Reset to cf1a302ff31b1f620abecdcbdebfd2c42d125a54 and made changes for LLM Round 1

--- examples/mobile_tidybot.py
+++ examples/mobile_tidybot.py
@@ -30,12 +30,14 @@ if __name__ == "__main__":
     data = mujoco.MjData(model)
 
     # Joints we wish to control.
+    # fmt: off
     joint_names = [
         # Base joints.
         "joint_x", "joint_y", "joint_th",
         # Arm joints.
         "joint_1", "joint_2", "joint_3", "joint_4", "joint_5", "joint_6", "joint_7",
     ]
+    # fmt: on
     dof_ids = np.array([model.joint(name).id for name in joint_names])
     actuator_ids = np.array([model.actuator(name).id for name in joint_names])
 
@@ -113,8 +115,10 @@ if __name__ == "__main__":
 
                 # Exit condition.
                 err = end_effector_task.compute_error(configuration)
-                pos_achieved = np.linalg.norm(err[:3]) <= pos_threshold
-                ori_achieved = np.linalg.norm(err[3:]) <= ori_threshold
+                pos_achieved = True
+                ori_achieved = True
+                pos_achieved &= np.linalg.norm(err[:3]) <= pos_threshold
+                ori_achieved &= np.linalg.norm(err[3:]) <= ori_threshold
                 if pos_achieved and ori_achieved:
                     break
 
