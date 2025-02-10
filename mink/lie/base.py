aa65cec47ebbaaf435fe19b127c6b37d63a8bad9
venkatkrishna <venkatkrishna@moe>
2025-02-10 04:39:09 +0100

    Reset to 5fbf0f6aeeaf1746af88137b9a299e84687a74ce and made changes for LLM Round 1

--- mink/lie/base.py
+++ mink/lie/base.py
@@ -33,7 +33,7 @@ class MatrixLieGroup(abc.ABC):
         assert isinstance(other, MatrixLieGroup)
         return self.multiply(other=other)
 
-    # Factory
+    # Factory.
 
     @classmethod
     @abc.abstractmethod
