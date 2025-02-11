3c96184bd8e1a7b6cae6f4460cb5253243851233
venkatkrishna <venkatkrishna@moe>
2025-02-11 03:06:44 +0100

    Reset to 5fbf0f6aeeaf1746af88137b9a299e84687a74ce and made changes for LLM Round 1

--- docs/conf.py
+++ docs/conf.py
@@ -4,41 +4,26 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "mink"
 copyright = "2024, Kevin Zakka"
 author = "Kevin Zakka"
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# List of Sphinx extensions to enable
 extensions = [
-    "sphinx.ext.autodoc",  # Automatically generate documentation from docstrings
-    "sphinx.ext.coverage",  # Check for documentation coverage
-    "sphinx-mathjax-offline",  # Use MathJax for rendering math equations offline
-    "sphinx.ext.napoleon",  # Support for Google and NumPy style docstrings
-    "sphinx_favicon",  # Add favicons to the documentation
+    "sphinx.ext.autodoc",
+    "sphinx.ext.coverage",
+    "sphinx-mathjax-offline",
+    "sphinx.ext.napoleon",
+    "sphinx_favicon",
 ]
 
-# Paths to templates that the theme can use
 templates_path = ["_templates"]
-
-# List of patterns to ignore when looking for source files
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
-# Mapping of source file suffixes to their corresponding parsers
 source_suffix = {".rst": "restructuredtext"}
-
-# Style for syntax highlighting in the documentation
 pygments_style = "sphinx"
 
-# Configuration for the Napoleon extension
-napoleon_numpy_docstring = False  # Do not use NumPy style docstrings
-napoleon_use_rtype = False  # Do not include the return type in the docstring
-
-# Configuration for the autodoc extension
 autodoc_typehints = "both"
 autodoc_class_signature = "separated"
 autodoc_default_options = {
@@ -47,19 +32,14 @@ autodoc_default_options = {
     "show-inheritance": True,
     "member-order": "bysource",
     "inherited-members": None,
-    "exclude-members": "__init__, __weakref__",
+    "exclude-members": "__init__,__weakref__",
 }
 
-# Type aliases for autodoc
 autodoc_type_aliases = {
     "npt.ArrayLike": "ArrayLike",
 }
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# Theme to use for HTML and HTML Help pages
 html_theme = "sphinx_rtd_theme"
-
-# Name of the HTML help builder output file
 htmlhelp_basename = "minkdoc"
