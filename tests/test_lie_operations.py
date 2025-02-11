b83080fd3cee1c5ae5de6e74b71b05f764494078
venkatkrishna <venkatkrishna@moe>
2025-02-11 06:24:06 +0100

    Reset to a69629a7fbeccaa8b60c30e9a385b9c1de01e3cf and made changes for LLM Round 1

--- tests/test_lie_operations.py
+++ tests/test_lie_operations.py
@@ -72,57 +72,77 @@ def check_jlog(state: MatrixLieGroup):
 )
 class TestOperations(parameterized.TestCase):
     def test_inverse_bijective(self, group: Type[MatrixLieGroup]):
+        """Check inverse of inverse."""
         transform = group.sample_uniform()
         check_inverse_bijective(transform)
 
     def test_matrix_bijective(self, group: Type[MatrixLieGroup]):
+        """Check that we can convert to and from matrices."""
         transform = group.sample_uniform()
         check_matrix_bijective(transform, group)
 
     def test_log_exp_bijective(self, group: Type[MatrixLieGroup]):
+        """Check 1-to-1 mapping for log <=> exp operations."""
         transform = group.sample_uniform()
         check_log_exp_bijective(transform, group)
 
     def test_adjoint(self, group: Type[MatrixLieGroup]):
+        """Check adjoint operation."""
         transform = group.sample_uniform()
         check_adjoint(transform, group)
 
     def test_rminus(self, group: Type[MatrixLieGroup]):
+        """Check rminus operation."""
         T_a = group.sample_uniform()
         T_b = group.sample_uniform()
         check_rminus(T_a, T_b)
 
     def test_lminus(self, group: Type[MatrixLieGroup]):
+        """Check lminus operation."""
         T_a = group.sample_uniform()
         T_b = group.sample_uniform()
         check_lminus(T_a, T_b)
 
     def test_rplus(self, group: Type[MatrixLieGroup]):
+        """Check rplus operation."""
         T_a = group.sample_uniform()
         T_b = group.sample_uniform()
         check_rplus(T_a, T_b)
 
     def test_lplus(self, group: Type[MatrixLieGroup]):
+        """Check lplus operation."""
         T_a = group.sample_uniform()
         T_b = group.sample_uniform()
         check_lplus(T_a, T_b)
 
     def test_jlog(self, group: Type[MatrixLieGroup]):
+        """Check jlog operation."""
         state = group.sample_uniform()
         check_jlog(state)
 
-    def test_rpy_bijective(self):
-        T = SO3.sample_uniform()
-        assert_transforms_close(T, SO3.from_rpy_radians(*T.as_rpy_radians()))
+
+class TestGroupSpecificOperations(absltest.TestCase):
+    """Group specific tests."""
+
+    @parameterized.named_parameters(
+        ("SO3", SO3),
+        ("SE3", SE3),
+    )
+    def test_rpy_bijective(self, group: Type[MatrixLieGroup]):
+        """Check RPY conversion is bijective for SO3."""
+        if group is SO3:
+            T = group.sample_uniform()
+            assert_transforms_close(T, group.from_rpy_radians(*T.as_rpy_radians()))
 
     def test_rpy_bijective_random(self):
+        """Check RPY conversion is bijective for SO3 with random samples."""
         T = SO3.sample_uniform()
         rpy = T.as_rpy_radians()
         T_reconstructed = SO3.from_rpy_radians(*rpy)
         assert_transforms_close(T, T_reconstructed)
 
     def test_rpy_bijective_edge_cases(self):
-        # Test edge cases for RPY conversion
+        """Check RPY conversion is bijective for SO3 with edge cases."""
         edge_cases = [
             (0, 0, 0),
             (np.pi/2, 0, 0),
@@ -138,6 +158,21 @@ class TestOperations(parameterized.TestCase):
             rpy = T.as_rpy_radians()
             T_reconstructed = SO3.from_rpy_radians(*rpy)
             assert_transforms_close(T, T_reconstructed)
+
+    def test_invalid_shape_log_exp(self):
+        """Check that log and exp raise errors for invalid shapes."""
+        T = SO3.sample_uniform()
+        invalid_tangent = np.random.rand(T.tangent_dim + 1)
+        with self.assertRaises(ValueError):
+            T.exp(invalid_tangent)
+        with self.assertRaises(ValueError):
+            T.log(invalid_tangent)
+
+    def test_copy_operation(self):
+        """Check that copying a transformation results in an identical transformation."""
+        T = SO3.sample_uniform()
+        T_copy = T.copy()
+        assert_transforms_close(T, T_copy)
 
 
 if __name__ == "__main__":
