ccd76bb786c3c0ae971bbb764ec98e675f0cba86
venkatkrishna <venkatkrishna@moe>
2025-02-11 01:12:22 +0100

    Reset to c9885f28a9f81f574c4cb89778f23937ac93622d and made changes for LLM Round 1

--- docs/conf.py
+++ docs/conf.py
@@ -1,11 +1,26 @@
 # Configuration file for the Sphinx documentation builder.
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# Project information
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import toml
+
+# Read project version from pyproject.toml
+with open("../pyproject.toml", "r") as file:
+    pyproject = toml.load(file)
+project_version = pyproject["tool"]["poetry"]["version"]
+
 project = "mink"
 copyright = "2024, Kevin Zakka"
 author = "Kevin Zakka"
+version = project_version
+release = project_version
 
-# General configuration
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.coverage",
@@ -14,8 +29,11 @@ extensions = [
     "sphinx_favicon",
 ]
 
+# Include both type hints in the signature and description
 autodoc_typehints = "both"
+# Separate class signature and docstring
 autodoc_class_signature = "separated"
+# Default options for autodoc
 autodoc_default_options = {
     "members": True,
     "member-order": "bysource",
@@ -23,17 +41,26 @@ autodoc_default_options = {
     "exclude-members": "__init__, __post_init__, __new__",
 }
 
+# Paths to templates
 templates_path = ["_templates"]
+# Patterns to exclude from source files
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
+# Source file suffixes
 source_suffix = {".rst": "restructuredtext"}
 
+# Pygments syntax highlighting style
 pygments_style = "sphinx"
 
+# Type aliases for autodoc
 autodoc_type_aliases = {
     "npt.ArrayLike": "ArrayLike",
 }
 
-# Options for HTML output
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# Theme for HTML output
 html_theme = "sphinx_rtd_theme"
+# Base name for HTML help builder output
 htmlhelp_basename = "minkdoc"
