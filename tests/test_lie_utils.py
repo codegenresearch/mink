0bdd234b2c71a7f7a37ed6798508e305fe47a3d5
venkatkrishna <venkatkrishna@moe>
2025-02-11 06:02:04 +0100

    Reset to 372c0d0807895860f5f90ac4cc1d0cae7b98f14c and made changes for LLM Round 1

--- tests/test_lie_utils.py
+++ tests/test_lie_utils.py
@@ -11,9 +11,29 @@ class TestUtils(absltest.TestCase):
         with self.assertRaises(AssertionError):
             utils.skew(np.zeros((5,)))
 
+    def test_skew_throws_assertion_error_if_input_not_1d(self):
+        with self.assertRaises(AssertionError):
+            utils.skew(np.zeros((3, 3)))
+
+    def test_skew_throws_assertion_error_if_input_not_numeric(self):
+        with self.assertRaises(AssertionError):
+            utils.skew(np.array(['a', 'b', 'c']))
+
     def test_skew_equals_negative(self):
         m = utils.skew(np.random.randn(3))
         np.testing.assert_allclose(m.T, -m)
+
+    def test_skew_returns_correct_skew_symmetric_matrix(self):
+        v = np.array([1, 2, 3])
+        expected = np.array([[0, -3, 2],
+                             [3, 0, -1],
+                             [-2, 1, 0]])
+        np.testing.assert_allclose(utils.skew(v), expected)
+
+    def test_skew_zero_vector_returns_zero_matrix(self):
+        v = np.zeros(3)
+        expected = np.zeros((3, 3))
+        np.testing.assert_allclose(utils.skew(v), expected)
 
 
 if __name__ == "__main__":
