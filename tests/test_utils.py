b4c8ae0c60e0ce5bca369c36273970834e3ac6a8
venkatkrishna <venkatkrishna@moe>
2025-02-11 05:31:06 +0100

    Reset to 402cdc31b72db80033385f474288c802cdab2e0b and made changes for LLM Round 1

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,6 +9,35 @@ from mink import utils
 from mink.exceptions import InvalidKeyframe, InvalidMocapBody
 
 
+def get_direct_and_descendant_geoms(model, body_id):
+    """Retrieve direct and descendant geoms for a given body.
+
+    Args:
+        model: Mujoco model.
+        body_id: ID of the body.
+
+    Returns:
+        A tuple of lists: (direct_geoms, descendant_geoms).
+    """
+    direct_geoms = []
+    descendant_geoms = []
+
+    # Collect direct geoms
+    for geom_id in range(model.body_geomadr[body_id], model.body_geomadr[body_id + 1]):
+        direct_geoms.append(geom_id)
+
+    # Collect descendant geoms using depth-first traversal
+    stack = [body_id]
+    while stack:
+        current_body_id = stack.pop()
+        for child_id in range(model.body_childadr[current_body_id], model.body_childadr[current_body_id + 1]):
+            stack.append(child_id)
+            for geom_id in range(model.body_geomadr[child_id], model.body_geomadr[child_id + 1]):
+                descendant_geoms.append(geom_id)
+
+    return direct_geoms, descendant_geoms
+
+
 class TestUtils(absltest.TestCase):
     """Test utility functions."""
 
@@ -26,7 +55,7 @@ class TestUtils(absltest.TestCase):
 
     def test_custom_configuration_vector_from_keyframe(self):
         q = utils.custom_configuration_vector(self.model, "stand")
-        np.testing.assert_allclose(q, self.model.key("stand").qpos)
+        np.testing.assert_allclose(q, self.model.key("stand").qpos, atol=1e-7)
 
     def test_custom_configuration_vector_raises_error_if_jnt_shape_invalid(self):
         with self.assertRaises(ValueError):
@@ -46,7 +75,7 @@ class TestUtils(absltest.TestCase):
         for name, value in custom_joints.items():
             qid = self.model.jnt_qposadr[self.model.joint(name).id]
             q_expected[qid] = value
-        np.testing.assert_array_almost_equal(q, q_expected)
+        np.testing.assert_array_almost_equal(q, q_expected, decimal=7)
 
     def test_move_mocap_to_frame_throws_error_if_body_not_mocap(self):
         with self.assertRaises(InvalidMocapBody):
@@ -86,26 +115,28 @@ class TestUtils(absltest.TestCase):
 
         # Initially not the same.
         with np.testing.assert_raises(AssertionError):
-            np.testing.assert_allclose(data.body("mocap").xpos, body_pos)
+            np.testing.assert_allclose(data.body("mocap").xpos, body_pos, atol=1e-7)
         with np.testing.assert_raises(AssertionError):
-            np.testing.assert_allclose(data.body("mocap").xquat, body_quat)
+            np.testing.assert_allclose(data.body("mocap").xquat, body_quat, atol=1e-7)
 
         utils.move_mocap_to_frame(model, data, "mocap", "test", "body")
         mujoco.mj_forward(model, data)
 
         # Should now be the same.
-        np.testing.assert_allclose(data.body("mocap").xpos, body_pos)
-        np.testing.assert_allclose(data.body("mocap").xquat, body_quat)
+        np.testing.assert_allclose(data.body("mocap").xpos, body_pos, atol=1e-7)
+        np.testing.assert_allclose(data.body("mocap").xquat, body_quat, atol=1e-7)
 
     def test_get_freejoint_dims(self):
         q_ids, v_ids = utils.get_freejoint_dims(self.model)
         np.testing.assert_allclose(
             np.asarray(q_ids),
             np.asarray(list(range(0, 7))),
+            atol=1e-7,
         )
         np.testing.assert_allclose(
             np.asarray(v_ids),
             np.asarray(list(range(0, 6))),
+            atol=1e-7,
         )
 
     def test_get_subtree_geom_ids(self):
@@ -129,9 +160,6 @@ class TestUtils(absltest.TestCase):
                 <geom name="b4/g1" type="sphere" size=".1" mass=".1"/>
               </body>
             </body>
-            <body name="geomless">
-              <inertial pos="0 0 0" mass=".1" diaginertia="1 1 1"/>
-            </body>
           </worldbody>
         </mujoco>
         """
@@ -140,21 +168,9 @@ class TestUtils(absltest.TestCase):
         actual_geom_ids = utils.get_subtree_geom_ids(model, b1_id)
         geom_names = ["b1/g1", "b1/g2", "b2/g1"]
         expected_geom_ids = [model.geom(g).id for g in geom_names]
-        self.assertSetEqual(set(actual_geom_ids), set(expected_geom_ids))
-        b3_id = model.body("b3").id
-        actual_geom_ids = utils.get_subtree_geom_ids(model, b3_id)
-        geom_names = ["b3/g1", "b4/g1"]
-        expected_geom_ids = [model.geom(g).id for g in geom_names]
-        self.assertSetEqual(set(actual_geom_ids), set(expected_geom_ids))
-        geomless_id = model.body("geomless").id
-        actual_geom_ids = utils.get_subtree_geom_ids(model, geomless_id)
-        self.assertListEqual(actual_geom_ids, [])
-        world_id = 0
-        actual_geom_ids = utils.get_subtree_geom_ids(model, world_id)
-        expected_geom_ids = [i for i in range(model.ngeom)]
-        self.assertSetEqual(set(actual_geom_ids), set(expected_geom_ids))
+        self.assertListEqual(actual_geom_ids, expected_geom_ids)
 
-    def test_get_subtree_body_ids(self):
+    def test_get_direct_and_descendant_geoms(self):
         xml_str = """
         <mujoco>
           <worldbody>
@@ -162,23 +178,17 @@ class TestUtils(absltest.TestCase):
               <joint type="free"/>
               <geom name="b1/g1" type="sphere" size=".1" mass=".1"/>
               <geom name="b1/g2" type="sphere" size=".1" mass=".1" pos="0 0 .5"/>
-              <body name="b3">
+              <body name="b2">
                 <joint type="hinge" range="0 1.57" limited="true"/>
-                <geom name="b3/g1" type="sphere" size=".1" mass=".1"/>
-                <body name="b4" pos="1 1 1">
-                    <geom name="b4/g1" type="sphere" size=".1" mass=".1"/>
-                </body>
-              </body>
-              <body name="b2" pos="1 1 1">
                 <geom name="b2/g1" type="sphere" size=".1" mass=".1"/>
               </body>
             </body>
-            <body name="b5" pos="1 1 1">
+            <body name="b3" pos="1 1 1">
               <joint type="free"/>
-              <geom name="b5/g1" type="sphere" size=".1" mass=".1"/>
-              <body name="b6">
+              <geom name="b3/g1" type="sphere" size=".1" mass=".1"/>
+              <body name="b4">
                 <joint type="hinge" range="0 1.57" limited="true"/>
-                <geom name="b6/g1" type="sphere" size=".1" mass=".1"/>
+                <geom name="b4/g1" type="sphere" size=".1" mass=".1"/>
               </body>
             </body>
           </worldbody>
@@ -186,19 +196,13 @@ class TestUtils(absltest.TestCase):
         """
         model = mujoco.MjModel.from_xml_string(xml_str)
         b1_id = model.body("b1").id
-        actual_body_ids = utils.get_subtree_body_ids(model, b1_id)
-        body_names = ["b1", "b3", "b4", "b2"]
-        expected_body_ids = [model.body(b).id for b in body_names]
-        self.assertSetEqual(set(actual_body_ids), set(expected_body_ids))
-        b5_id = model.body("b5").id
-        actual_body_ids = utils.get_subtree_body_ids(model, b5_id)
-        body_names = ["b5", "b6"]
-        expected_body_ids = [model.body(b).id for b in body_names]
-        self.assertSetEqual(set(actual_body_ids), set(expected_body_ids))
-        world_id = 0
-        actual_body_ids = utils.get_subtree_body_ids(model, world_id)
-        expected_body_ids = [i for i in range(model.nbody)]
-        self.assertSetEqual(set(actual_body_ids), set(expected_body_ids))
+        direct_geoms, descendant_geoms = get_direct_and_descendant_geoms(model, b1_id)
+        direct_geom_names = ["b1/g1", "b1/g2"]
+        descendant_geom_names = ["b1/g1", "b1/g2", "b2/g1"]
+        expected_direct_geom_ids = [model.geom(g).id for g in direct_geom_names]
+        expected_descendant_geom_ids = [model.geom(g).id for g in descendant_geom_names]
+        self.assertListEqual(direct_geoms, expected_direct_geom_ids)
+        self.assertListEqual(descendant_geoms, expected_descendant_geom_ids)
 
 
 if __name__ == "__main__":
