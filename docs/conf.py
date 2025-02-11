4875554d9f23df12ee562e727354fbcd968581f7
venkatkrishna <venkatkrishna@moe>
2025-02-11 01:11:44 +0100

    Reset to 5fbf0f6aeeaf1746af88137b9a299e84687a74ce and made changes for LLM Round 1

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 # Configuration file for the Sphinx documentation builder.
 #
 # This file configures the Sphinx documentation generator to build the documentation for the mink project.
@@ -10,16 +8,16 @@ from __future__ import annotations
 # This section contains metadata about the project, which is used in various parts of the generated documentation.
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project: str = "mink"
-copyright: str = "2024, Kevin Zakka"
-author: str = "Kevin Zakka"
+project = "mink"
+copyright = "2024, Kevin Zakka"
+author = "Kevin Zakka"
 
 # -- General Configuration ---------------------------------------------------
 # This section configures general settings for the Sphinx documentation builder.
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 # List of Sphinx extensions to enable
-extensions: list[str] = [
+extensions = [
     "sphinx.ext.autodoc",  # Automatically generate documentation from docstrings
     "sphinx.ext.coverage",  # Check for documentation coverage
     "sphinx-mathjax-offline",  # Use MathJax for rendering math equations offline
@@ -28,27 +26,39 @@ extensions: list[str] = [
 ]
 
 # Paths to templates that the theme can use
-templates_path: list[str] = ["_templates"]
+templates_path = ["_templates"]
 
 # List of patterns to ignore when looking for source files
-exclude_patterns: list[str] = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # Mapping of source file suffixes to their corresponding parsers
-source_suffix: dict[str, str] = {".rst": "restructuredtext"}
+source_suffix = {".rst": "restructuredtext"}
 
 # Style for syntax highlighting in the documentation
-pygments_style: str = "sphinx"
+pygments_style = "sphinx"
 
 # Configuration for the Napoleon extension
-napoleon_numpy_docstring: bool = False  # Do not use NumPy style docstrings
-napoleon_use_rtype: bool = False  # Do not include the return type in the docstring
+napoleon_numpy_docstring = False  # Do not use NumPy style docstrings
+napoleon_use_rtype = False  # Do not include the return type in the docstring
+
+# Configuration for the autodoc extension
+autodoc_typehints = "description"
+autodoc_class_signature = "separated"
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": True,
+    "show-inheritance": True,
+}
+
+# Type aliases for autodoc
+autodoc_type_aliases = {}
 
 # -- Options for HTML Output -------------------------------------------------
 # This section configures the HTML output options for the Sphinx documentation builder.
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 # Theme to use for HTML and HTML Help pages
-html_theme: str = "sphinx_rtd_theme"
+html_theme = "sphinx_rtd_theme"
 
 # Name of the HTML help builder output file
-htmlhelp_basename: str = "minkdoc"
+htmlhelp_basename = "minkdoc"
