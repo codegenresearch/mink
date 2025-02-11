d3f66eab9f0c02117ab67855482d5b280b743cb3
venkatkrishna <venkatkrishna@moe>
2025-02-11 07:45:22 +0100

    Reset to e861235d28d5598fac82c90aaec11c9d4218cc3c and made changes for LLM Round 1

--- examples/quadruped_spot.py
+++ examples/quadruped_spot.py
@@ -95,7 +95,7 @@ if __name__ == "__main__":
 
                 pos_achieved = True
                 ori_achieved = True
-                for task in [eef_task, base_task, *feet_tasks]:
+                for task in tasks:
                     err = task.compute_error(configuration)
                     pos_achieved &= bool(np.linalg.norm(err[:3]) <= pos_threshold)
                     ori_achieved &= bool(np.linalg.norm(err[3:]) <= ori_threshold)
@@ -105,6 +105,5 @@ if __name__ == "__main__":
             data.ctrl = configuration.q[7:]
             mujoco.mj_step(model, data)
 
-            # Visualize at fixed FPS.
             viewer.sync()
             rate.sleep()
