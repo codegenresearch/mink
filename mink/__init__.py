0ee5c3bd025cc5fd92dc8df6b978b3817f24035a
venkatkrishna <venkatkrishna@moe>
2025-02-11 05:30:58 +0100

    Reset to 402cdc31b72db80033385f474288c802cdab2e0b and made changes for LLM Round 1

--- mink/__init__.py
+++ mink/__init__.py
@@ -39,7 +39,6 @@ from .utils import (
     custom_configuration_vector,
     get_body_geom_ids,
     get_freejoint_dims,
-    get_subtree_body_ids,
     get_subtree_geom_ids,
     move_mocap_to_frame,
 )
@@ -75,12 +74,9 @@ __all__ = (
     "FRAME_TO_JAC_FUNC",
     "FRAME_TO_POS_ATTR",
     "FRAME_TO_XMAT_ATTR",
-    "set_mocap_pose_from_frame",
-    "pose_from_mocap",
     "custom_configuration_vector",
     "get_freejoint_dims",
     "move_mocap_to_frame",
     "get_subtree_geom_ids",
     "get_body_geom_ids",
-    "get_subtree_body_ids",
 )
