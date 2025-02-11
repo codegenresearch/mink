defca5ebfc859af264605f49b8f5d255595e1427
venkatkrishna <venkatkrishna@moe>
2025-02-11 03:07:44 +0100

    Reset to e861235d28d5598fac82c90aaec11c9d4218cc3c and made changes for LLM Round 1

--- tests/test_lie_utils.py
+++ tests/test_lie_utils.py
@@ -7,11 +7,11 @@ from mink.lie import utils
 
 
 class TestUtils(absltest.TestCase):
-    def test_skew_raises_assertion_error_for_invalid_shape(self):
+    def test_skew_invalid_shape_raises_assertion(self):
         with self.assertRaises(AssertionError):
             utils.skew(np.zeros((5,)))
 
-    def test_skew_matrix_transpose_is_negative_of_original(self):
+    def test_skew_transpose_is_negative(self):
         m = utils.skew(np.random.randn(3))
         np.testing.assert_allclose(m.T, -m)
 
