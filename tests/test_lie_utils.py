4b99a596b5eb4fd2dee69e8ea99290e9a45a0b7b
venkatkrishna <venkatkrishna@moe>
2025-02-11 06:24:54 +0100

    Reset to e861235d28d5598fac82c90aaec11c9d4218cc3c and made changes for LLM Round 1

--- tests/test_lie_utils.py
+++ tests/test_lie_utils.py
@@ -6,15 +6,15 @@ from absl.testing import absltest
 from mink.lie import utils
 
 
-class TestUtilityFunctions(absltest.TestCase):
-    def test_skew_raises_assertion_error_for_invalid_shape(self):
+class TestUtils(absltest.TestCase):
+    def test_skew_invalid_shape(self):
         with self.assertRaises(AssertionError):
             utils.skew(np.zeros((5,)))
 
-    def test_skew_matrix_transpose_equals_negative(self):
-        random_vector = np.random.randn(3)
-        skew_matrix = utils.skew(random_vector)
-        np.testing.assert_allclose(skew_matrix.T, -skew_matrix)
+    def test_skew_transpose_negative(self):
+        x = np.random.randn(3)
+        m = utils.skew(x)
+        np.testing.assert_allclose(m.T, -m)
 
 
 if __name__ == "__main__":
