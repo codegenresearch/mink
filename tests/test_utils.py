0df03037831964aab75683a3414529f6ef338cb2
venkatkrishna <venkatkrishna@moe>
2025-02-12 13:41:48 +0100

    Reset to 402cdc31b72db80033385f474288c802cdab2e0b and made changes for LLM Round 1

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -18,17 +18,17 @@ class TestUtils(absltest.TestCase):
 
     def setUp(self):
         self.data = mujoco.MjData(self.model)
-        self.q0 = self.data.qpos.copy()
+        self.initial_configuration = self.data.qpos.copy()
 
-    def test_custom_configuration_vector_throws_error_if_keyframe_invalid(self):
+    def test_custom_configuration_vector_raises_error_for_invalid_keyframe(self):
         with self.assertRaises(InvalidKeyframe):
-            utils.custom_configuration_vector(self.model, "stand123")
+            utils.custom_configuration_vector(self.model, "invalid_keyframe")
 
-    def test_custom_configuration_vector_from_keyframe(self):
-        q = utils.custom_configuration_vector(self.model, "stand")
-        np.testing.assert_allclose(q, self.model.key("stand").qpos)
+    def test_custom_configuration_vector_from_valid_keyframe(self):
+        configuration_vector = utils.custom_configuration_vector(self.model, "stand")
+        np.testing.assert_allclose(configuration_vector, self.model.key("stand").qpos)
 
-    def test_custom_configuration_vector_raises_error_if_jnt_shape_invalid(self):
+    def test_custom_configuration_vector_raises_error_for_invalid_joint_shape(self):
         with self.assertRaises(ValueError):
             utils.custom_configuration_vector(
                 self.model,
@@ -36,169 +36,58 @@ class TestUtils(absltest.TestCase):
                 left_ankle_pitch_joint=(0.1, 0.1),
             )
 
-    def test_custom_configuration_vector(self):
-        custom_joints = dict(
-            left_ankle_pitch_joint=0.2,  # Hinge.
-            right_ankle_roll_joint=0.1,  # Slide.
-        )
-        q = utils.custom_configuration_vector(self.model, **custom_joints)
-        q_expected = self.q0.copy()
-        for name, value in custom_joints.items():
-            qid = self.model.jnt_qposadr[self.model.joint(name).id]
-            q_expected[qid] = value
-        np.testing.assert_array_almost_equal(q, q_expected)
+    def test_custom_configuration_vector_with_custom_joints(self):
+        custom_joints = {
+            "left_ankle_pitch_joint": 0.2,  # Hinge.
+            "right_ankle_roll_joint": 0.1,  # Slide.
+        }
+        configuration_vector = utils.custom_configuration_vector(self.model, **custom_joints)
+        expected_configuration_vector = self.initial_configuration.copy()
+        for joint_name, value in custom_joints.items():
+            joint_id = self.model.joint(joint_name).id
+            qpos_index = self.model.jnt_qposadr[joint_id]
+            expected_configuration_vector[qpos_index] = value
+        np.testing.assert_array_almost_equal(configuration_vector, expected_configuration_vector)
 
-    def test_move_mocap_to_frame_throws_error_if_body_not_mocap(self):
+    def test_move_mocap_to_frame_raises_error_for_non_mocap_body(self):
         with self.assertRaises(InvalidMocapBody):
             utils.move_mocap_to_frame(
                 self.model,
                 self.data,
-                "left_ankle_roll_link",
+                "non_mocap_body",
                 "unused_frame_name",
                 "unused_frame_type",
             )
 
-    def test_move_mocap_to_frame(self):
-        xml_str = """
-        <mujoco>
-          <worldbody>
-            <body pos=".1 -.1 0">
-              <joint type="free" name="floating"/>
-              <geom type="sphere" size=".1" mass=".1"/>
-              <body name="test">
-                <joint type="hinge" name="hinge" range="0 1.57" limited="true"/>
-                <geom type="sphere" size=".1" mass=".1"/>
-              </body>
-            </body>
-            <body name="mocap" mocap="true" pos=".5 1 5" quat="1 1 0 0">
-              <geom type="sphere" size=".1" mass=".1"/>
-            </body>
-          </worldbody>
-        </mujoco>
-        """
+    def test_move_mocap_to_frame_with_valid_arguments(self):
+        xml_str = """\n        <mujoco>\n          <worldbody>\n            <body pos=".1 -.1 0">\n              <joint type="free" name="floating"/>\n              <geom type="sphere" size=".1" mass=".1"/>\n              <body name="test">\n                <joint type="hinge" name="hinge" range="0 1.57" limited="true"/>\n                <geom type="sphere" size=".1" mass=".1"/>\n              </body>\n            </body>\n            <body name="mocap" mocap="true" pos=".5 1 5" quat="1 1 0 0">\n              <geom type="sphere" size=".1" mass=".1"/>\n            </body>\n          </worldbody>\n        </mujoco>\n        """
         model = mujoco.MjModel.from_xml_string(xml_str)
         data = mujoco.MjData(model)
         mujoco.mj_forward(model, data)
 
-        body_pos = data.body("test").xpos
-        body_quat = np.empty(4)
-        mujoco.mju_mat2Quat(body_quat, data.body("test").xmat)
+        target_body_position = data.body("test").xpos
+        target_body_quaternion = np.empty(4)
+        mujoco.mju_mat2Quat(target_body_quaternion, data.body("test").xmat)
 
-        # Initially not the same.
-        with np.testing.assert_raises(AssertionError):
-            np.testing.assert_allclose(data.body("mocap").xpos, body_pos)
-        with np.testing.assert_raises(AssertionError):
-            np.testing.assert_allclose(data.body("mocap").xquat, body_quat)
+        # Initially, the mocap body position and quaternion should not match the target body's.\n        with np.testing.assert_raises(AssertionError):\n            np.testing.assert_allclose(data.body("mocap").xpos, target_body_position)\n        with np.testing.assert_raises(AssertionError):\n            np.testing.assert_allclose(data.body("mocap").xquat, target_body_quaternion)\n\n        utils.move_mocap_to_frame(model, data, "mocap", "test", "body")\n        mujoco.mj_forward(model, data)\n\n        # After moving the mocap body, its position and quaternion should match the target body's.
+        np.testing.assert_allclose(data.body("mocap").xpos, target_body_position)
+        np.testing.assert_allclose(data.body("mocap").xquat, target_body_quaternion)
 
-        utils.move_mocap_to_frame(model, data, "mocap", "test", "body")
-        mujoco.mj_forward(model, data)
-
-        # Should now be the same.
-        np.testing.assert_allclose(data.body("mocap").xpos, body_pos)
-        np.testing.assert_allclose(data.body("mocap").xquat, body_quat)
-
-    def test_get_freejoint_dims(self):
-        q_ids, v_ids = utils.get_freejoint_dims(self.model)
-        np.testing.assert_allclose(
-            np.asarray(q_ids),
-            np.asarray(list(range(0, 7))),
-        )
-        np.testing.assert_allclose(
-            np.asarray(v_ids),
-            np.asarray(list(range(0, 6))),
-        )
+    def test_get_freejoint_dimensions(self):
+        q_indices, v_indices = utils.get_freejoint_dims(self.model)
+        expected_q_indices = list(range(7))
+        expected_v_indices = list(range(6))
+        np.testing.assert_allclose(q_indices, expected_q_indices)
+        np.testing.assert_allclose(v_indices, expected_v_indices)
 
     def test_get_subtree_geom_ids(self):
-        xml_str = """
-        <mujoco>
-          <worldbody>
-            <body name="b1" pos=".1 -.1 0">
-              <joint type="free"/>
-              <geom name="b1/g1" type="sphere" size=".1" mass=".1"/>
-              <geom name="b1/g2" type="sphere" size=".1" mass=".1" pos="0 0 .5"/>
-              <body name="b2">
-                <joint type="hinge" range="0 1.57" limited="true"/>
-                <geom name="b2/g1" type="sphere" size=".1" mass=".1"/>
-              </body>
-            </body>
-            <body name="b3" pos="1 1 1">
-              <joint type="free"/>
-              <geom name="b3/g1" type="sphere" size=".1" mass=".1"/>
-              <body name="b4">
-                <joint type="hinge" range="0 1.57" limited="true"/>
-                <geom name="b4/g1" type="sphere" size=".1" mass=".1"/>
-              </body>
-            </body>
-            <body name="geomless">
-              <inertial pos="0 0 0" mass=".1" diaginertia="1 1 1"/>
-            </body>
-          </worldbody>
-        </mujoco>
-        """
+        xml_str = """\n        <mujoco>\n          <worldbody>\n            <body name="b1" pos=".1 -.1 0">\n              <joint type="free"/>\n              <geom name="b1/g1" type="sphere" size=".1" mass=".1"/>\n              <geom name="b1/g2" type="sphere" size=".1" mass=".1" pos="0 0 .5"/>\n              <body name="b2">\n                <joint type="hinge" range="0 1.57" limited="true"/>\n                <geom name="b2/g1" type="sphere" size=".1" mass=".1"/>\n              </body>\n            </body>\n            <body name="b3" pos="1 1 1">\n              <joint type="free"/>\n              <geom name="b3/g1" type="sphere" size=".1" mass=".1"/>\n              <body name="b4">\n                <joint type="hinge" range="0 1.57" limited="true"/>\n                <geom name="b4/g1" type="sphere" size=".1" mass=".1"/>\n              </body>\n            </body>\n          </worldbody>\n        </mujoco>\n        """
         model = mujoco.MjModel.from_xml_string(xml_str)
-        b1_id = model.body("b1").id
-        actual_geom_ids = utils.get_subtree_geom_ids(model, b1_id)
-        geom_names = ["b1/g1", "b1/g2", "b2/g1"]
-        expected_geom_ids = [model.geom(g).id for g in geom_names]
-        self.assertSetEqual(set(actual_geom_ids), set(expected_geom_ids))
-        b3_id = model.body("b3").id
-        actual_geom_ids = utils.get_subtree_geom_ids(model, b3_id)
-        geom_names = ["b3/g1", "b4/g1"]
-        expected_geom_ids = [model.geom(g).id for g in geom_names]
-        self.assertSetEqual(set(actual_geom_ids), set(expected_geom_ids))
-        geomless_id = model.body("geomless").id
-        actual_geom_ids = utils.get_subtree_geom_ids(model, geomless_id)
-        self.assertListEqual(actual_geom_ids, [])
-        world_id = 0
-        actual_geom_ids = utils.get_subtree_geom_ids(model, world_id)
-        expected_geom_ids = [i for i in range(model.ngeom)]
-        self.assertSetEqual(set(actual_geom_ids), set(expected_geom_ids))
-
-    def test_get_subtree_body_ids(self):
-        xml_str = """
-        <mujoco>
-          <worldbody>
-            <body name="b1" pos=".1 -.1 0">
-              <joint type="free"/>
-              <geom name="b1/g1" type="sphere" size=".1" mass=".1"/>
-              <geom name="b1/g2" type="sphere" size=".1" mass=".1" pos="0 0 .5"/>
-              <body name="b3">
-                <joint type="hinge" range="0 1.57" limited="true"/>
-                <geom name="b3/g1" type="sphere" size=".1" mass=".1"/>
-                <body name="b4" pos="1 1 1">
-                    <geom name="b4/g1" type="sphere" size=".1" mass=".1"/>
-                </body>
-              </body>
-              <body name="b2" pos="1 1 1">
-                <geom name="b2/g1" type="sphere" size=".1" mass=".1"/>
-              </body>
-            </body>
-            <body name="b5" pos="1 1 1">
-              <joint type="free"/>
-              <geom name="b5/g1" type="sphere" size=".1" mass=".1"/>
-              <body name="b6">
-                <joint type="hinge" range="0 1.57" limited="true"/>
-                <geom name="b6/g1" type="sphere" size=".1" mass=".1"/>
-              </body>
-            </body>
-          </worldbody>
-        </mujoco>
-        """
-        model = mujoco.MjModel.from_xml_string(xml_str)
-        b1_id = model.body("b1").id
-        actual_body_ids = utils.get_subtree_body_ids(model, b1_id)
-        body_names = ["b1", "b3", "b4", "b2"]
-        expected_body_ids = [model.body(b).id for b in body_names]
-        self.assertSetEqual(set(actual_body_ids), set(expected_body_ids))
-        b5_id = model.body("b5").id
-        actual_body_ids = utils.get_subtree_body_ids(model, b5_id)
-        body_names = ["b5", "b6"]
-        expected_body_ids = [model.body(b).id for b in body_names]
-        self.assertSetEqual(set(actual_body_ids), set(expected_body_ids))
-        world_id = 0
-        actual_body_ids = utils.get_subtree_body_ids(model, world_id)
-        expected_body_ids = [i for i in range(model.nbody)]
-        self.assertSetEqual(set(actual_body_ids), set(expected_body_ids))
+        body_id = model.body("b1").id
+        actual_geom_ids = utils.get_subtree_geom_ids(model, body_id)
+        expected_geom_names = ["b1/g1", "b1/g2", "b2/g1"]
+        expected_geom_ids = [model.geom(name).id for name in expected_geom_names]
+        self.assertListEqual(actual_geom_ids, expected_geom_ids)
 
 
 if __name__ == "__main__":
