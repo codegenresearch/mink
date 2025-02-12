436bc3dad6a4ad931a31f57934f86db567a545ee
venkatkrishna <venkatkrishna@moe>
2025-02-12 13:41:36 +0100

    Reset to e861235d28d5598fac82c90aaec11c9d4218cc3c and made changes for LLM Round 1

--- tests/test_task.py
+++ tests/test_task.py
@@ -1,23 +1,23 @@
-"""Tests for task.py."""
-
 from absl.testing import absltest
 
 from mink.tasks.exceptions import InvalidDamping, InvalidGain
 from mink.tasks.task import Task
 
 
-class TestTask(absltest.TestCase):
-    """Test abstract base class for tasks."""
+class TaskTest(absltest.TestCase):
+    """Test the Task abstract base class."""
 
     def setUp(self):
-        """Prepare test fixture."""
+        """Set up test fixtures."""
         Task.__abstractmethods__ = set()
 
-    def test_task_throws_error_if_gain_negative(self):
+    def test_task_raises_invalid_gain_error_when_gain_is_negative(self):
+        """Test that Task raises InvalidGain when gain is negative."""
         with self.assertRaises(InvalidGain):
             Task(cost=0.0, gain=-0.5)
 
-    def test_task_throws_error_if_lm_damping_negative(self):
+    def test_task_raises_invalid_damping_error_when_lm_damping_is_negative(self):
+        """Test that Task raises InvalidDamping when lm_damping is negative."""
         with self.assertRaises(InvalidDamping):
             Task(cost=0.0, gain=1.0, lm_damping=-1.0)
 
