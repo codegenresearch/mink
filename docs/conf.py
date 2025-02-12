63b3d6abbb5cd37a7184fcff83512745bf86e789
venkatkrishna <venkatkrishna@moe>
2025-02-12 13:40:39 +0100

    Reset to c9885f28a9f81f574c4cb89778f23937ac93622d and made changes for LLM Round 1

--- docs/conf.py
+++ docs/conf.py
@@ -1,29 +1,12 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# Configuration for the Sphinx documentation builder.
+# For more details, see https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-from pathlib import Path
-
-import toml
-
+# Project information
 project = "mink"
 copyright = "2024, Kevin Zakka"
 author = "Kevin Zakka"
 
-# The short X.Y version
-version: str = toml.load(Path(__file__).absolute().parent.parent / "pyproject.toml")[
-    "project"
-]["version"]
-if not version.isalpha():
-    version = "v" + version
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
+# General configuration
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.coverage",
@@ -43,18 +26,13 @@ autodoc_default_options = {
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
 source_suffix = {".rst": "restructuredtext"}
-
 pygments_style = "sphinx"
 
 autodoc_type_aliases = {
     "npt.ArrayLike": "ArrayLike",
 }
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
+# HTML output options
 html_theme = "sphinx_rtd_theme"
-
 htmlhelp_basename = "minkdoc"
